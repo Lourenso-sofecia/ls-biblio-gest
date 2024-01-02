a186fbd9fb1e46f746e181e54909c23dd727fb3a
sofecia <lourenso.sofecia@gmail.com>
2024-01-02 22:30:13 +0100

    style AdminLayout changed

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -10,7 +10,7 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
 
   return (
     <main className="flex px-4 lg:px-10 xl:px-20 2xl:px-40">
-      <ul className="flex flex-col gap-4 p-5 h-[500px] w-52 border-r border-black border-opacity-20 bg-white">
+      <ul className="flex flex-col gap-4 p-5 py-8 h-[500px] w-52 border-r border-black border-opacity-20 bg-white">
         <Link
           href={"/admin/users"}
           className={`flex w-full rounded-xl px-4 py-1 uppercase ${pathname?.includes("/admin/users") && "bg-yellow-900  text-white opacity-60"} `}
@@ -30,7 +30,7 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
           Livros
         </Link>
       </ul>
-      <div className="px-4 pt-5">{children}</div>
+      <div className="p-8">{children}</div>
     </main>
   );
 }
