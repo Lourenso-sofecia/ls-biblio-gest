ef65f797c1996b159693a7c72597780677caf247
sofecia <lourenso.sofecia@gmail.com>
2024-01-02 22:15:49 +0100

    style AdminLayout changed

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -10,22 +10,22 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
 
   return (
     <main className="flex px-4 lg:px-10 xl:px-20 2xl:px-40">
-      <ul className="flex flex-col gap-4 p-5 h-[500px] w-44 border-r border-black border-opacity-20 bg-white">
+      <ul className="flex flex-col gap-4 p-5 h-[500px] w-52 border-r border-black border-opacity-20 bg-white">
         <Link
           href={"/admin/users"}
-          className={`flex w-full rounded-xl px-4 py-1 ${pathname?.includes("/admin/users") && "bg-yellow-900  text-white opacity-60"} `}
+          className={`flex w-full rounded-xl px-4 py-1 uppercase ${pathname?.includes("/admin/users") && "bg-yellow-900  text-white opacity-60"} `}
         >
           Users
         </Link>
         <Link
           href={"/admin/employee"}
-          className={`flex w-full rounded-xl px-4 py-1 ${pathname?.includes("/admin/employee") && "bg-yellow-900  text-white opacity-60"} `}
+          className={`flex w-full rounded-xl px-4 py-1 uppercase ${pathname?.includes("/admin/employee") && "bg-yellow-900  text-white opacity-60"} `}
         >
           Funcionarios
         </Link>
         <Link
           href={"/admin/books"}
-          className={`flex w-full rounded-xl px-4 py-1 ${pathname?.includes("/admin/books") && "bg-yellow-900  text-white opacity-60"} `}
+          className={`flex w-full rounded-xl px-4 py-1 uppercase ${pathname?.includes("/admin/books") && "bg-yellow-900  text-white opacity-60"} `}
         >
           Livros
         </Link>
